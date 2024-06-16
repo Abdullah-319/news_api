65da8d56b78f91bf257cf075d982b8b3afa897a9
Abdullah Ahmad <abdullahahmad319.319@gmail.com>
2024-06-17 00:40:07 +0500

    make splash screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:news_api/view/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +13,15 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 2), () {
+      MaterialPageRoute(builder: (context) => const HomeScreen());
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.sizeOf(context).height * 1;
