b24f9462cf20b6a14b7175e928d4b0d076051311
Abdullah Ahmad <abdullahahmad319.319@gmail.com>
2024-06-17 00:38:00 +0500

    make splash screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,6 +12,37 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final height = MediaQuery.sizeOf(context).height * 1;
+    final width = MediaQuery.sizeOf(context).width * 1;
+
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'images/splash_pic.jpg',
+              filterQuality: FilterQuality.high,
+              fit: BoxFit.cover,
+              height: height * 0.5,
+              width: width,
+            ),
+            SizedBox(height: height * 0.04),
+            Text(
+              "TOP HEADLINES",
+              style: GoogleFonts.poppins(
+                letterSpacing: 0.6,
+                color: Colors.grey.shade700,
+              ),
+            ),
+            SizedBox(height: height * 0.04),
+            const SpinKitChasingDots(
+              color: Colors.blue,
+              size: 40,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
