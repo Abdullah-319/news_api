3d6755fd469459c6ca2cb78eb2a5c5b533486988
Abdullah Ahmad <abdullahahmad319.319@gmail.com>
2024-06-18 20:22:31 +0500

    fetch headlines from api

---
+++ lib/repository/headlines_repositoty.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:news_api/models/news_channel_headlines_model.dart';
+
+class HeadlinesRepositoty {
+  Future<NewsChannelHeadlinesModel> fetchNewsHeadlinesApi() async {
+    String url =
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=888ef9840bcb4bc7bcba2d3c5da79ccb";
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final body = jsonDecode(response.body);
+      return NewsChannelHeadlinesModel.fromJson(body);
+    }
+    throw Exception('Error');
+  }
+}
