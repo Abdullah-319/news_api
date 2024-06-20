f29743ae7216d09c50e5362a9fb03e8e4463b0c2
Abdullah Ahmad <abdullahahmad319.319@gmail.com>
2024-06-20 22:31:56 +0500

    make categories list view

--- lib/view/categories_screen.dart
+++ lib/view/categories_screen.dart
@@ -9,8 +9,22 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+  String category = "General";
+
+  List<String> categoriesList = [
+    "General",
+    "Entertainment",
+    "Health",
+    "Sports",
+    "Business",
+    "Technology",
+  ];
+
   @override
   Widget build(BuildContext context) {
+    final height = MediaQuery.sizeOf(context).height;
+    final width = MediaQuery.sizeOf(context).width;
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -28,7 +42,50 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
           ),
         ),
       ),
-      body: ,
+      body: Column(
+        children: [
+          SizedBox(
+            height: height * 0.055,
+            width: width,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categoriesList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: height * 0.004),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          category = categoriesList[index];
+                        });
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: height * 0.01, horizontal: width * 0.05),
+                        alignment: Alignment.center,
+                        decoration: const BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                        child: Text(
+                          categoriesList[index],
+                          style: GoogleFonts.poppins(
+                            color: categoriesList[index] == category
+                                ? Colors.white
+                                : Colors.black54,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
     );
   }
 }
